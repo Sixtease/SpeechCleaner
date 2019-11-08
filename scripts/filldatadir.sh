2b5686356218de2fa75bc6bfc89badca5f8bb08b
Oldřich Krůza <kruza@ufal.mff.cuni.cz>
2019-11-08 17:54:28 +0100

    add script to fill data directory with audio file symlinks

---
+++ scripts/filldatadir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+oklist="$1"
+srcdir="$2"
+destdir="$3"
+
+usage() {
+    echo "usage: $0 OKlist SrcDir DestDir"
+    exit 1
+}
+
+if [ -z "$oklist" ]; then usage; fi;
+if [ -z "$srcdir" ]; then usage; fi;
+if [ -z "$destdir" ]; then usage; fi;
+
+while read line; do
+    item=`echo "$line" | sed 's/#.*//'`
+    if echo "$item" | grep -vq '.'; then continue; fi
+    file=`ls "$srcdir/$item"*`
+    if [ -z "$file" ]; then continue; fi
+    echo $file
+    ln -s "$file"* "$destdir";
+done < "$oklist"
